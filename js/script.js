50b20897a9aa4b9c7ad9b5a36d586151b6f71933
Casey Cook <cook.caseya@gmail.com>
2020-03-22 20:42:44 -0400

    added services cards, font awesome icons, etc.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(".card1").hover(function(){
+  $(".card1 p").css("color", "white");
+  }, function(){
+  $(".card1 p").css("color", "black");
+});
+
+$(".card2").hover(function(){
+  $(".card2 p").css("color", "white");
+  }, function(){
+  $(".card2 p").css("color", "black");
+});
+
+$(".card3").hover(function(){
+  $(".card3 p").css("color", "white");
+  }, function(){
+  $(".card3 p").css("color", "black");
+});
+
+$(".card4").hover(function(){
+  $(".card4 p").css("color", "white");
+  }, function(){
+  $(".card4 p").css("color", "black");
+});
+
+$(".card5").hover(function(){
+  $(".card5 p").css("color", "white");
+  }, function(){
+  $(".card5 p").css("color", "black");
+});
+
+$(".card6").hover(function(){
+  $(".card6 p").css("color", "white");
+  }, function(){
+  $(".card6 p").css("color", "black");
+});
